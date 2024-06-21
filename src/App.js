bdf916ce88fb6b41d70470cb194023d484848cc3
gortegas <gortegas@nttdata.com>
2024-06-21 11:44:12 +0200

    Pinta bien

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.png';
 import './App.css';
-import { InputGroup, Form, Container, Row, Col, Button } from 'react-bootstrap';
+import { InputGroup, Form, Container, Row, Col, Alert } from 'react-bootstrap';
 import { useState, useEffect } from 'react';
 
 function App() {
 
   const [nCuotas, setNCuotas] = useState(1);
   const [ini, setIni] = useState({ cantidad: 0, cuota: 0 });
-  const [cuotas, setCuotas] = useState([]);
+  const [cuotas, setCuotas] = useState([{ value: 0, result: 0 }]);
   const [res, setRes] = useState({ apostado: 0, ganado: 0 });
+  const [refresh, setRefresh] = useState(true);
 
   const abc = ["A", "B", "C", "D", "E", "F"]
 
@@ -19,12 +19,20 @@ function App() {
   }, [nCuotas]);
 
   useEffect(() => {
-    let apostado = cuotas.map((c) => c.result).reduce((partialSum, a) => partialSum + a, 0) + ini.cantidad;
+    let apostado = parseFloat(cuotas.map((c) => c.result).reduce((partialSum, a) => parseFloat(partialSum) + parseFloat(a), 0)) + parseFloat(ini.cantidad);
     setRes({
       apostado: parseFloat(apostado).toFixed(2),
       ganado: parseFloat(ini.cantidad * ini.cuota - apostado).toFixed(2)
     })
-  }, [cuotas,ini])
+  }, [cuotas, ini])
+
+  useEffect(() => {
+    try {
+      calcular()
+    } catch (error) {
+
+    }
+  }, [refresh])
 
 
   const handleChangeCuotaValue = (index, e) => {
@@ -32,6 +40,7 @@ function App() {
       idx === index ? { ...input, value: e } : input
     );
     setCuotas(newInputs);
+    setRefresh(!refresh);
   };
 
   function updateNCuotas(number) {
@@ -46,11 +55,11 @@ function App() {
 
   function calcular() {
     const nerdamer = require("nerdamer/all.min")
-    let temp=[];
-    if (cuotas.length===1){
-      let one=ini.cantidad/(cuotas[0].value-1).toFixed(2);
-      temp.push({value:cuotas[0].value,result:one});
-    }else{
+    let temp = [];
+    if (cuotas.length === 1) {
+      let one = (ini.cantidad / (cuotas[0].value - 1)).toFixed(2);
+      temp.push({ value: cuotas[0].value, result: one });
+    } else if (cuotas.length > 0) {
       let arr = cuotas.map((item, index) => {
         let rest = [];
         for (let j = 0; j <= cuotas.length - 1; j++) {
@@ -58,13 +67,13 @@ function App() {
             rest.push(j);
           }
         }
-        return (1 - item.value) + abc[index] + rest.map((it) => "+" + abc[it]).join("") + "+" + ini.cantidad +"=0" 
+        return (1 - item.value) + abc[index] + rest.map((it) => "+" + abc[it]).join("") + "+" + ini.cantidad + "=0"
       });
       console.log(arr);
       var sol = nerdamer.solveEquations(arr);
       console.log(sol);
       for (let j = 0; j <= cuotas.length - 1; j++) {
-        temp.push({value:cuotas[j].value,result:parseFloat(sol[j][1]).toFixed(2)})
+        temp.push({ value: cuotas[j].value, result: parseFloat(sol[j][1]).toFixed(2) })
       }
     }
     setCuotas(temp);
@@ -74,10 +83,14 @@ function App() {
     <Container fluid className="App-header">
       <Row className='pad'>
         <Col>
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1>
-            Calculadora de cuotas
-          </h1>
+          <img src={"/logo" + (Math.round(Math.random() * 3) + 1) + ".png"} className="App-logo" alt="logo" />
+        </Col>
+      </Row>
+      <Row className='pad'>
+        <Col>
+        <Alert key={"info"} variant={"info"}>
+          Calculadora de cuotas
+        </Alert>
         </Col>
       </Row>
       <Row className='pad'>
@@ -91,13 +104,13 @@ function App() {
           <InputGroup.Text>x</InputGroup.Text>
           <Form.Control aria-label="cuota" type="number" placeholder='Cuota' min="1" onChange={(e) => setIni({ ...ini, cuota: e.target.value })} />
           <InputGroup.Text>=</InputGroup.Text>
-          <Form.Control type="number" disabled value={ini.cuota * ini.cantidad} />
+          <Form.Control type="number" disabled value={(ini.cuota * ini.cantidad).toFixed()} />
         </InputGroup>
       </Row>
       <Row className='pad'>
         <Col>
           {cuotas.map((input, index) => (
-            <InputGroup>
+            <InputGroup key={index}>
               <Form.Control aria-label="catidad" disabled readOnly placeholder='Cantidad' default={input.result} value={input.result} />
               <InputGroup.Text>x</InputGroup.Text>
               <Form.Control aria-label="cuota" type="number" value={input.value} placeholder='Rellename' onChange={(e) => handleChangeCuotaValue(index, e.target.value)} />
@@ -105,21 +118,21 @@ function App() {
           ))}
         </Col>
       </Row>
-      {res.apostado !== 0 ?
+      {parseFloat(res.apostado) !== 0 ?
         <Row className="pad">
           <InputGroup>
-              <InputGroup.Text>Apostado:</InputGroup.Text>
-              <InputGroup.Text>{res.apostado}</InputGroup.Text>
-            </InputGroup>
+            <InputGroup.Text>Apostado:</InputGroup.Text>
+            <InputGroup.Text>{res.apostado}</InputGroup.Text>
+          </InputGroup>
           <InputGroup>
-              <InputGroup.Text>Ganado:</InputGroup.Text>
-              <InputGroup.Text>{res.ganado}</InputGroup.Text>
-            </InputGroup>
+            <InputGroup.Text>Ganado:</InputGroup.Text>
+            <InputGroup.Text>{res.ganado}</InputGroup.Text>
+          </InputGroup>
         </Row>
         : <></>}
-      <Row className="pad">
+      {/* <Row className="pad">
           <Button onClick={() => calcular()}>Calcular</Button>
-        </Row>
+        </Row> */}
     </Container>
   );
 }
